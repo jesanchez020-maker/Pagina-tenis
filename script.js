eb00e75fbd311199415bbaa2db9039cc5e3a5f97
jesanchez020-maker <je.sanchez020@ebc.edu.mx>
2026-09-22 17:17:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,261 +1,257 @@
-/* =========================================
-   FOOTBALLZONE - PRODUCTOS
-========================================= */
-
 const products = [
 
-    /* ==============================
-       ADIDAS
-    ============================== */
+    // =========================
+    // ADIDAS
+    // =========================
 
     {
         id: 1,
         brand: "Adidas",
-        name: "F50 Elite",
+        name: "F50 Elite FG",
         price: 7499,
-        image: "https://soccerworld.com.au/cdn/shop/files/JH7617_1_FOOTWEAR_Photography_SideLateralCenterView_white.jpg?v=1747024225"
+        image: "https://soccerworld.com.au/cdn/shop/files/JH7615_1_FOOTWEAR_Photography_Side-Lateral-Center-View_white.jpg?v=1751276772&width=416"
     },
 
     {
         id: 2,
         brand: "Adidas",
-        name: "F50 Club",
-        price: 1699,
-        image: "https://www.nike.com/mx/t/tacos-de-futbol-para-terreno-firme-de-corte-low-nike-mercurial-vapor-17-elite-TX31XXVo"
+        name: "F50 League",
+        price: 2199,
+        image: "https://soccerworld.com.au/cdn/shop/files/JH7615_5_FOOTWEAR_Photography_FrontLateralTopView_white.jpg?v=1751908545&width=416"
     },
 
     {
         id: 3,
         brand: "Adidas",
-        name: "F50 Club Messi",
-        price: 1799,
-        image: "https://www.google.com/imgres?q=F50%20Club%20Messi&imgurl=https%3A%2F%2Fassets.adidas.com%2Fimages%2Fw_500%2Cf_auto%2Cq_auto%2F650f2ef6a0e54c04b9d8dc99826f3c66_9366%2FTacos_de_Futbol_F50_Club_Messi_Terreno_Firme-Multiterreno_Beige_IH1913_01_00_standard_hover.jpg&imgrefurl=https%3A%2F%2Fwww.adidas.mx%2Ftacos-de-futbol-f50-club-messi-terreno-firme-multiterreno%2FIH1913.html&docid=OcZlFiTVVFAtVM&tbnid=VEJ3Ru3QImvtvM&vet=12ahUKEwj5hevj34KXAxU6D0QIHVuNID8QnPAOegQIOxAA..i&w=500&h=500&hcb=2&ved=2ahUKEwj5hevj34KXAxU6D0QIHVuNID8QnPAOegQIOxAA"
+        name: "F50 League Messi",
+        price: 2299,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_1_FOOTWEAR_Photography_Side-Lateral-Center-View_white.jpg?v=1781835136&width=416"
     },
 
     {
         id: 4,
         brand: "Adidas",
-        name: "F50 Hyperfast Elite",
-        price: 7499,
-        image: "https://www.futbolemotion.com/imagesarticulos/150000/1500000_0_1.jpg"
+        name: "F50 Pro TF",
+        price: 2999,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_2_FOOTWEAR_Photography_TopPortraitView_white.jpg?v=1781835177&width=416"
     },
 
     {
         id: 5,
         brand: "Adidas",
-        name: "F50 Hyperfast Club",
-        price: 1699,
-        image: "https://assets.adidas.com/images/w_1880%2Cf_auto%2Cq_auto/117be454d9414c6abf94693bfaf158c1_9366/JR9055_22_model.jpg"
+        name: "Predator Elite FG",
+        price: 7499,
+        image: "https://soccerworld.com.au/cdn/shop/files/Predator_24_Elite_Turf_Boots_Black_IG7731_01_standard.png?v=1735968783&width=416"
     },
 
     {
         id: 6,
         brand: "Adidas",
-        name: "F50 Sparkfusion Elite",
-        price: 6999,
-        image: "https://assets.adidas.com/images/w_1880%2Cf_auto%2Cq_auto/117be454d9414c6abf94693bfaf158c1_9366/JR9055_22_model.jpg"
+        name: "Predator League",
+        price: 2199,
+        image: "https://soccerworld.com.au/cdn/shop/files/Predator_24_Elite_Turf_Boots_Black_IG7731_02_standard.png?v=1735968783&width=416"
     },
 
     {
         id: 7,
         brand: "Adidas",
-        name: "Copa Pure 3 Elite",
-        price: 6999,
-        image: "https://assets.adidas.com/images/w_1880%2Cf_auto%2Cq_auto/ID9037_22_model.jpg"
+        name: "Copa Pure Elite",
+        price: 5999,
+        image: "https://soccerworld.com.au/cdn/shop/files/Predator_24_Elite_Turf_Boots_Black_IG7731_03_standard.png?v=1735968783&width=416"
     },
 
     {
         id: 8,
         brand: "Adidas",
-        name: "Copa Pure Club",
-        price: 1799,
-        image: "https://assets.adidas.com/images/w_1880%2Cf_auto%2Cq_auto/ID9037_22_model.jpg"
+        name: "Copa Pure League",
+        price: 2299,
+        image: "https://soccerworld.com.au/cdn/shop/files/Predator_24_Elite_Turf_Boots_Black_IG7731_04_standard.png?v=1735968784&width=416"
     },
 
     {
         id: 9,
         brand: "Adidas",
-        name: "Predator Elite",
-        price: 7499,
-        image: "https://assets.adidas.com/images/w_1880%2Cf_auto%2Cq_auto/IF8868_22_model.jpg"
+        name: "F50 Club",
+        price: 1699,
+        image: "https://soccerworld.com.au/cdn/shop/files/Predator_24_Elite_Turf_Boots_Black_IG7731_05_standard.png?v=1735968784&width=416"
     },
 
     {
         id: 10,
         brand: "Adidas",
-        name: "Predator Elite Sin Agujetas",
+        name: "F50 Hyperfast Elite",
         price: 7499,
-        image: "https://assets.adidas.com/images/w_1880%2Cf_auto%2Cq_auto/IF8868_22_model.jpg"
+        image: "https://soccerworld.com.au/cdn/shop/files/Predator_24_Elite_Turf_Boots_Black_IG7731_06_standard.png?v=1735968784&width=416"
     },
 
 
-    /* ==============================
-       NIKE
-    ============================== */
+    // =========================
+    // NIKE
+    // =========================
 
     {
         id: 11,
         brand: "Nike",
         name: "Mercurial Superfly 11 Elite",
-        price: 7899,
-        image: "https://www.nike.com/mx/t/tacos-de-futbol-para-terrenos-multiples-de-corte-high-nike-mercurial-superfly-11-academy-CHu6feUa"
+        price: 7599,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/1dcc9cca-39b5-4463-8af4-94b5a9ec2961/ZM+SUPERFLY+11+ELITE+FG+KM.png"
     },
 
     {
         id: 12,
         brand: "Nike",
         name: "Mercurial Superfly 11 Academy",
-        price: 2499,
-        image: "https://www.nike.com/mx/t/tacos-de-futbol-para-terrenos-multiples-de-corte-high-nike-mercurial-superfly-11-academy-CHu6feUa"
+        price: 2599,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/81dca9e9-4bc4-4162-9519-5e26eda94b8b/SUPERFLY+11+ACADEMY+TF+KM.png"
     },
 
     {
         id: 13,
         brand: "Nike",
         name: "Mercurial Superfly 11 Club",
-        price: 1899,
-        image: "https://static.nike.com/a/images/t_web_pdp_535_v2/f_auto%2Cu_9ddf04c7-2a9a-4d76-add1-d15af8f0263d%2Cc_scale%2Cfl_relative%2Cw_1.0%2Ch_1.0%2Cfl_layer_apply/9781f26d-8e67-4b9c-b83d-83e0f20c0df8/VAPOR%2B17%2BPRO%2BFG%2BT.png"
+        price: 1999,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/e0c70da7-5696-4683-876f-7efa10fbe21a/SUPERFLY+11+CLUB+TF.png"
     },
 
     {
         id: 14,
         brand: "Nike",
-        name: "Mercurial Vapor 17 Elite",
-        price: 6999,
-        image: "https://www.futbolemotion.com/imagesarticulos/150000/1500000_0_1.jpg"
+        name: "Phantom 6 Low Club",
+        price: 1999,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/0e813658-c7ff-4e41-a9db-9722f1eba7b0/PHANTOM+6+LOW+CLUB+TF+T.png"
     },
 
     {
         id: 15,
         brand: "Nike",
-        name: "Mercurial Vapor 17 Pro",
-        price: 3699,
-        image: "https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwed7c0acd/images/hi-res/198729745910_5_20260528-mrtPeru.jpg"
+        name: "Phantom 6 High Club",
+        price: 1999,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/81866844-dfd7-499f-88f9-b2eb5a167137/PHANTOM+6+HIGH+CLUB+TF.png"
     },
 
     {
         id: 16,
         brand: "Nike",
-        name: "Mercurial Vapor 17 Academy",
-        price: 2399,
-        image: "https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwed7c0acd/images/hi-res/198729745910_5_20260528-mrtPeru.jpg"
+        name: "Phantom 6 Low Pro",
+        price: 3699,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/ab0b9f75-7ac4-4da1-99e6-72ddaf8dc883/REACTX+PHANTOM+6+LOW+PRO+TF+T.png"
     },
 
     {
         id: 17,
         brand: "Nike",
-        name: "Mercurial Vapor 17 Club",
-        price: 1699,
-        image: "https://www.nike.com.pe/on/demandware.static/-/Sites-catalog-equinox/default/dwed7c0acd/images/hi-res/198729745910_5_20260528-mrtPeru.jpg"
+        name: "Phantom 6 High Elite",
+        price: 6999,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/997638b7-170b-4072-97d2-1294d2117fac/PHANTOM+6+HIGH+ELITE+FG+T.png"
     },
 
     {
         id: 18,
         brand: "Nike",
-        name: "Phantom 6 High Elite",
-        price: 6599,
-        image: "https://cdn.sportshop.com/catalog/product/1500/1500/2/1/214845_1.jpg?v=2c2a8efab8806ce8"
+        name: "Phantom 6 High Club FG",
+        price: 2199,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/1257cf3f-1adb-458c-9560-66ee0cd360e5/PHANTOM+6+HIGH+CLUB+FG%2FMG.png"
     },
 
     {
         id: 19,
         brand: "Nike",
-        name: "Phantom 6 Low Pro",
-        price: 3199,
-        image: "https://cdn.sportshop.com/catalog/product/1500/1500/2/1/214845_1.jpg?v=2c2a8efab8806ce8"
+        name: "Phantom 6 Low Club FG",
+        price: 2199,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/20be608c-de64-45bc-8b44-306f9fc0c252/PHANTOM+6+LOW+CLUB+FG%2FMG.png"
     },
 
     {
         id: 20,
         brand: "Nike",
-        name: "Phantom 6 Low Academy",
-        price: 2199,
-        image: "https://cdn.sportshop.com/catalog/product/1500/1500/2/1/214845_1.jpg?v=2c2a8efab8806ce8"
+        name: "Tiempo Maestro Club",
+        price: 1799,
+        image: "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_386,c_limit/u_9ddf04c7-2a9a-4d76-add1-d15af8f0263d,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/da5b33e2-4641-4325-8bc5-45cde328254c/TIEMPO+MAESTRO+CLUB+TF+T.png"
     },
 
 
-    /* ==============================
-       PUMA
-    ============================== */
+    // =========================
+    // PUMA
+    // =========================
 
     {
         id: 21,
-        brand: "PUMA",
-        name: "ULTRA 6 CARBON FG",
+        brand: "Puma",
+        name: "Ultra 6 Carbon",
         price: 6999,
-        image: "https://us.puma.com/us/es/pd/tacos-de-futbol-unisex-ultra-6-carbon-dare-to-fg/108754?srsltid=AU7gw4V-ZvvgZCDWUfQu5Z3BKVoHGa2XvC5TQTB1GjvjUJhl5IHJbg9l"
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_3_FOOTWEAR_Photography_BottomView_white.jpg?v=1781835177&width=416"
     },
 
     {
         id: 22,
-        brand: "PUMA",
-        name: "ULTRA 6 PRO FG/AG",
-        price: 3399,
-        image: "https://mx.puma.com/mx/es/pd/tacos-de-futbol-ultra-6-pro-fg-ag-para-adolescentes/108698.html?srsltid=AU7gw4VXUHFyWAVQC8-Tj5g33jon3bzq2COrSCU5_XEtA2XtXTCr1mt5"
+        brand: "Puma",
+        name: "Ultra 6 Pro",
+        price: 3999,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_4_FOOTWEAR_Photography_SideMedialCenterView_white.jpg?v=1781835177&width=416"
     },
 
     {
         id: 23,
-        brand: "PUMA",
-        name: "ULTRA 6 MATCH MG",
-        price: 2149,
-        image: "https://www.forumsport.com/images/puma-botas-de-futbol-cesped-artificial-ultra-6-match-mg-az-lateral-exterior-1001048040-500x500-f"
+        brand: "Puma",
+        name: "Ultra 6 Match",
+        price: 2999,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_5_FOOTWEAR_Photography_FrontLateralTopView_white.jpg?v=1781835177&width=416"
     },
 
     {
         id: 24,
-        brand: "PUMA",
-        name: "ULTRA 6 PLAY FG/AG",
-        price: 1299,
-        image: "https://www.forumsport.com/images/puma-botas-de-futbol-cesped-artificial-ultra-6-match-mg-az-lateral-exterior-1001048040-500x500-f"
+        brand: "Puma",
+        name: "Ultra 6 Play",
+        price: 2199,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_6_FOOTWEAR_Photography_BackLateralTopView_white.jpg?v=1781835177&width=416"
     },
 
     {
         id: 25,
-        brand: "PUMA",
-        name: "ULTRA NITRO 7 MATCH",
+        brand: "Puma",
+        name: "Future 9 Play",
         price: 2199,
-        image: "https://www.google.com/imgres?q=ULTRA%20NITRO%207%20MATCH&imgurl=https%3A%2F%2Fsp203.liverpool.com.mx%2Fi%2F1202400597.jpg&imgrefurl=https%3A%2F%2Fwww.liverpool.com.mx%2Ftienda%2Fpdp%2Ftenis-ultra-nitro-7-match-fgag-para-futbol-de-hombre%2F1202400589&docid=O-IUJJK1g9Q_pM&tbnid=zF7OBmVJiAjuGM&vet=12ahUKEwiDtMfD44KXAxW1DkQIHRZYLnoQnPAOegQITBAA..i&w=940&h=1215&hcb=2&ved=2ahUKEwiDtMfD44KXAxW1DkQIHRZYLnoQnPAOegQITBAA"
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_7_FOOTWEAR_Photography_DetailView1_white.jpg?v=1781835177&width=416"
     },
 
     {
         id: 26,
-        brand: "PUMA",
-        name: "FUTURE 9 PLAY",
-        price: 1299,
-        image: "https://www.liverpool.com.mx/tienda/pdp/future-9-play-para-f-tbol-de-ni%C3%B1o/999674130302"
+        brand: "Puma",
+        name: "Future 9 Match",
+        price: 2999,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_8_FOOTWEAR_Photography_DetailView2_white.jpg?v=1781835177&width=416"
     },
 
     {
         id: 27,
-        brand: "PUMA",
-        name: "FUTURE 9 MATCH",
-        price: 1899,
-        image: "https://www.dportenis.mx/Taquetes-Puma-Future-9-Match-FG-Hombre-108713-03?srsltid=AU7gw4WWTqUkwSSaRH1ocJ0MxMG89t-p0RqUEVZsurzVUR0oCT-0OUEZ"
+        brand: "Puma",
+        name: "Future 9 Pro",
+        price: 4299,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_14_FOOTWEAR_Photography_HeroMrktngView1.jpg?v=1781835200&width=416"
     },
 
     {
         id: 28,
-        brand: "PUMA",
-        name: "FUTURE 9 ULTIMATE",
-        price: 6099,
-        image: "https://www.google.com/imgres?q=prodirectsoccer%20puma&imgurl=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0781%2F2900%2F4760%2Ffiles%2F4000461_main.jpg%3Fv%3D1789758608%26width%3D800&imgrefurl=https%3A%2F%2Fwww.prodirectsport.es%2Fcollections%2Fcoleccion-botas-puma-arrival%3Fsrsltid%3DAU7gw4UsOiL6-db1SzWvgQx0l1B4zRfo1AfbStTLMWHwPgxE3cMusJrp&docid=dgzJLSeCDVuU-M&tbnid=lGjfyDdAkXLskM&vet=12ahUKEwik37OCo4OXAxVx_8kDHe4HC7gQnPAOegQIPRAA..i&w=800&h=800&hcb=2&ved=2ahUKEwik37OCo4OXAxVx_8kDHe4HC7gQnPAOegQIPRAA"
+        brand: "Puma",
+        name: "King Ultimate",
+        price: 4999,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_16_FOOTWEAR_Photography_HeroMrktngView5.jpg?v=1781835200&width=416"
     },
 
     {
         id: 29,
-        brand: "PUMA",
-        name: "KING 20 MATCH",
-        price: 2099,
-        image: "https://www.google.com/imgres?q=prodirectsoccer%20puma&imgurl=https%3A%2F%2Flookaside.instagram.com%2Fseo%2Fgoogle_widget%2Fcrawler%2F%3Fmedia_id%3D3977868344229113458&imgrefurl=https%3A%2F%2Fwww.instagram.com%2Fp%2FDc0Omp-jMsR%2F&docid=WmwuWDrXXLETUM&tbnid=w85qhL4WWt1MgM&vet=12ahUKEwik37OCo4OXAxVx_8kDHe4HC7gQnPAOegUIvAEQAA..i&w=1080&h=1350&hcb=2&itg=1&ved=2ahUKEwik37OCo4OXAxVx_8kDHe4HC7gQnPAOegUIvAEQAA"
+        brand: "Puma",
+        name: "King Match",
+        price: 2999,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_18_FOOTWEAR_Photography_HeroMrktngView7.jpg?v=1781835200&width=416"
     },
 
     {
         id: 30,
-        brand: "PUMA",
-        name: "KING ULTIMATE",
-        price: 5999,
-        image: "https://www.prodirectsport.com/cdn/shop/files/1035470_main.jpg?v=1789124429&width=1200"
+        brand: "Puma",
+        name: "King Pro",
+        price: 3799,
+        image: "https://soccerworld.com.au/cdn/shop/files/IH1892_19_FOOTWEAR_Photography_HeroMrktngView8.jpg?v=1781835200&width=416"
     }
 
 ];
